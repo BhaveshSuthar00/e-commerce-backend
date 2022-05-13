780241ea2b541b1292a4302801b7babf32461b37
BhaveshSuthar00 <bhaveshs0770@gmail.com>
2022-05-13 21:07:20 +0530

    added the session express to the backend for cookies

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const User = require('../models/user.model');
-
 const router = express.Router();
 const { authenticate } = require("../middlewares/authorization");
 
--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 
 const cors = require('cors');
 
 const cookieParser = require('cookie-parser');
-
+const session = require('express-session');
 const port = process.env.PORT || 2200;
 
 const app = express();
@@ -16,11 +17,11 @@ const productController = require('./controllers/product.controller');
 
 const { authenticate } = require('./middlewares/authorization');
 
-app.use(cors({credentials : true, origin: 'http://localhost:3000'}));
-
+// app.use(cors({credentials : true, origin: 'http://localhost:3000'}));
+app.use(cors({credentials: true, origin : "http://localhost:3000"}))
 app.use(express.json());
 
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 function updateRequestMethod(req, res, next) {
     if (req.body.method) {
@@ -31,6 +32,18 @@ function updateRequestMethod(req, res, next) {
 }
 
 app.use(updateRequestMethod);
+app.set("trust proxy", 1);
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET || 'Super Secret (change it)',
+        resave: true,
+        saveUninitialized: false,
+        cookie: {
+            sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax', // must be 'none' to enable cross-site delivery
+            secure: process.env.NODE_ENV === "production", // must be true if sameSite='none'
+        }
+    })
+);
 // app.use('/', async (req, res)=>{
 //     return res.status(200).send({message: 'Success'})
 // })
