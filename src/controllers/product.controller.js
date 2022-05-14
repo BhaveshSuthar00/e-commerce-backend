ec186de5404611a4d43939844482fc26260e79e1
BhaveshSuthar00 <bhaveshs0770@gmail.com>
2022-05-14 18:04:45 +0530

    add

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 router.post('/post', async(req, res)=>{
     try {
-        const data = await Product.create(req.body);
-        return res.status(200).json(data);
+        const Products = await Product.create(req.body);
+        return res.status(200).json({message: 'Product successfully created', product : Products});
     }
     catch (e) {
         return res.status(404).json({message : e});
